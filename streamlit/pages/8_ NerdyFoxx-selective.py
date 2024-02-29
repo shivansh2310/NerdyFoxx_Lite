44b600c3f37330a18ae7f04a0fee61f415108d51
shivansh2310 <42717219+shivansh2310@users.noreply.github.com>
2024-03-01 00:11:55 +0530

    Final

--- streamlit/pages/8_ NerdyFoxx-selective.py
+++ streamlit/pages/8_ NerdyFoxx-selective.py
@@ -1,12 +1,84 @@
 import streamlit as st
 
-st.set_page_config(page_title="Week 6", layout="wide", page_icon='6️⃣')
+st.set_page_config(page_title="NerdyFoxx-Selective", layout="wide", page_icon='6️⃣')
 
 st.markdown("##  Week 6️⃣: NerdyFoxx-Selective")
 
+st.markdown("---")
 
-st.markdown("""### ⭐ Coming Soon...
-""", unsafe_allow_html=True)
+st.markdown("### 97% Owned: The Cruel Truth Behind Money Credit and Financial Crisis | ENDEVR Documentary")
+
+st.video("https://www.youtube.com/embed/j7-FmAfxMmQ?rel=0")
+
+
+st.markdown("---")
+
+st.markdown("### Fabulous Documentary About Business Creativity in China - Hanfu Trading - Experts on China")
+
+st.video("https://www.youtube.com/embed/B2xThboVwDM?rel=0")
+
+st.markdown("---")
+
+st.markdown("### How Corporations Are Ruining Your Health (Food Industry Documentary) | Real Stories")
+
+st.video("https://www.youtube.com/embed/5RwIWGkMxrE?rel=0")
+
+st.markdown("---")
+
+st.markdown("### The Darkest Scandal in NFL History | Paid to Kill | NFL Documentary")
+
+st.video("https://www.youtube.com/embed/N4oL9x20aS0?rel=0")
+
+st.markdown("---")
+
+st.markdown("### Shah - Rukh | Happiest Man In The World | Award Winning Indian Documentary Film | Six Sigma Films")
+
+st.video("https://www.youtube.com/embed/y57cKWlqHCE?rel=0")
+
+st.markdown("---")
+
+st.markdown("### Know Everything About Earth - Evolution of Life On Earth - Documentary for Knowledge")
+
+st.video("https://www.youtube.com/embed/viqeIv3nZxg?list=PL0v8QCI-Squ6U23kLhzDYB6_mpFyJqqA-")
+
+st.markdown("---")
+
+st.markdown("### The Strongest Material Ever Made on Earth New Documentary")
+
+st.video("https://www.youtube.com/embed/kmNvb6M2o-I?list=PL0v8QCI-Squ6U23kLhzDYB6_mpFyJqqA-")
+
+st.markdown("---")
+
+st.markdown("### Future Humanoid Robots Will Blow your Mind 2016 Full Documentary")
+
+st.video("https://www.youtube.com/embed/nJSgks3svnU?list=PL0v8QCI-Squ6U23kLhzDYB6_mpFyJqqA-")
+
+st.markdown("---")
+
+st.markdown("### Know Everything About MARS, NASA And CURIOSITY ROVER Project Documentary for Knowledge")
+
+st.video("https://www.youtube.com/embed/yN4q35e9XEQ?list=PL0v8QCI-Squ6U23kLhzDYB6_mpFyJqqA-")
+
+st.markdown("---")
+
+st.markdown("### Who Killed Capitalism? (Global Crash Documentary) | Real Stories")
+
+st.video("https://www.youtube.com/embed/MPSkhgXMPJ8?list=PLBPwM9uB5E5-s9NbW9IsncFsHhN8QbX8-")
+
+st.markdown("---")
+
+st.markdown("### Banking On Africa - The Bitcoin Revolution (full documentary) - True Story")
+
+st.video("https://www.youtube.com/embed/_VT_zpHQzBE?list=PLBPwM9uB5E5-s9NbW9IsncFsHhN8QbX8-")
+
+st.markdown("---")
+
+st.markdown("### The Secret Bank Bailout (HD 1080p) | German TV Award 2013")
+
+st.video("https://www.youtube.com/embed/xu5sTyAXyAo?list=PLBPwM9uB5E5-s9NbW9IsncFsHhN8QbX8-")
+
+st.markdown("---")
+
 
 
 
